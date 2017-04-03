42694d6bed1fff850e8976ca2fbe55a5b613bf58
johny_smith <iburlachenko2013@gmail.com>
2017-04-03 11:45:13 +0300

    initial commit of project

---
+++ README.md
@@ -0,0 +1,1 @@
+# hal-run description here
---
+++ run.py
@@ -0,0 +1,46 @@
+import os
+from flask import Flask, request, render_template
+import shlex
+from subprocess import Popen, PIPE
+
+root_dir = '/home/ivan/hal-run/'
+music_dir = root_dir + '/static/music'
+
+app = Flask(__name__)
+
+
+def get_exitcode_stdout_stderr(cmd):
+    """
+    Execute the external command and get its exitcode, stdout and stderr.
+    """
+    args = shlex.split(cmd)
+
+    proc = Popen(args, stdout=PIPE, stderr=PIPE)
+    out, err = proc.communicate()
+    exitcode = proc.returncode
+    return exitcode, out, err
+
+@app.route('/')
+@app.route('/home')
+def index():
+    music_files = [f for f in os.listdir(music_dir) if f.endswith('mp3')]
+    music_files_number = len(music_files)
+    return render_template("index.html",
+                        title = 'Home',
+                        music_files_number = music_files_number,
+                        music_files = music_files)
+
+@app.route('/arc')
+def start_Arc_effect():
+    cmd = "./static/music/arc_demo"
+    get_exitcode_stdout_stderr(cmd);
+    
+
+@app.route('/<filename>')
+def song(filename):
+    return render_template('play.html',
+                        title = filename,
+                        music_file = filename)
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', debug = True)
