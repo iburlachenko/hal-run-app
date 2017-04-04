c88ca48cc7e2acdce2ffc192e50b3d5b26e04ace
johny_smith <iburlachenko2013@gmail.com>
2017-04-04 23:28:15 +0300

    recursion fixed

--- run.py
+++ run.py
@@ -40,6 +40,8 @@ def index():
 
 @app.route('/ef/<effect_name>')
 def start_effect(effect_name=None):
+    print('-new eff->' + effect_name)
+    print('-new eff->' + app.config['curr_effect'])
     if (effect_name != app.config['curr_effect']):
         clearEffects()
         app_name = effect_name
